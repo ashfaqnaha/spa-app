3afc737beb84b964e0dfc4e6ea453a8c714b67fd
ashfaqnaha <ashfaqnaha2000@gmail.com>
2024-09-15 19:12:16 +0530

    live

--- app/_components/callButton.js
+++ app/_components/callButton.js
@@ -6,7 +6,7 @@ const CallButton = ({ phoneNumber }) => {
       href={`tel:${phoneNumber}`}
       className="text-white px-4 py-2 rounded-md frosted-glass"
     >
-      Call
+      Call : +919633699962
     </a>
   );
 };
