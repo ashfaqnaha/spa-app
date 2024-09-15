bda5aef7f35b06dcb95102c4a5496578342f5394
ashfaqnaha <ashfaqnaha2000@gmail.com>
2024-09-15 18:51:18 +0530

    live

---
+++ app/_components/jobButton.js
@@ -0,0 +1,14 @@
+'use client'
+
+const JobButton = () => {
+  return (
+    <a
+      href={`https://forms.gle/1ZcWhUG3awyDMArm7`}
+      className="text-white px-4 py-2 rounded-md frosted-glass"
+    >
+      Apply for Beautician Vacancy
+    </a>
+  );
+};
+
+export default JobButton;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { EB_Garamond,Montserrat,Bodoni_Moda, } from "next/font/google";
 import Image from "next/image";
 import MapButton from './_components/mapButton' 
 import CallButton from './_components/callButton'
-import JobButton from './_components/jobButton'
+// import JobButton from './_components/jobButton'
 
 const garamond = EB_Garamond({ subsets: ["latin"] });
 const garamondy = EB_Garamond({ subsets: ["latin"] });
@@ -21,7 +21,7 @@ export default function Home() {
           <h1 className={`${bodoni.className} hidden md:block flex justify-center text-9xl `}>greendot</h1>
           {/* <h1 className={`${montserrat.className} mt-8 md:mt-0 flex justify-center text-2xl`}>HAIR&nbsp;&nbsp;&nbsp;      SKIN&nbsp;&nbsp;&nbsp;        MAKEUP</h1> */}
           
-          <div className="flex mt-10 justify-center"><CallButton phoneNumber="+919605888498"/>&nbsp;&nbsp;&nbsp;<MapButton/>&nbsp;&nbsp;&nbsp;<JobButton/></div>
+          <div className="flex mt-10 justify-center"><CallButton phoneNumber="+919605888498"/>&nbsp;&nbsp;&nbsp;<MapButton/></div>
           {/* <div className="flex mt-10 justify-center"><JobButton/></div> */}
 
       </div>
